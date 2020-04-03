742b8478499bc8fa2e070cc963efe6f62448a098
Lukas Hirsch <hirschlukasa@gmail.com>
2020-04-03 14:52:17 -0500

    took out of dev

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+
+  app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+  });
